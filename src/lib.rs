78a183f25a5005bd6429cad7dedb628625d8c925
Avi ד <avi.the.coder@gmail.com>
2018-12-16 19:46:48 -0500

    Impl PerfectHasher for u8, u16, u32, u64, usize

--- src/lib.rs
+++ src/lib.rs
@@ -4,58 +4,68 @@ use std::hash::{Hash, Hasher};
 
 use nohash_hasher::{BuildNoHashHasher, IntMap};
 
-pub struct PerfectHasher<C, H> {
+pub struct PerfectHasher<Id, C, H> {
     // Key is the Id
-    alloted: IntMap<u32, C>,
+    alloted: IntMap<Id, C>,
     hasher: H,
 }
 
-impl<C, H> PerfectHasher<C, H>
-where
-    H: Hasher,
-    C: Hash + Ord,
-{
-    pub fn new(hasher: H) -> Self {
-        PerfectHasher {
-            alloted: IntMap::default(),
-            hasher,
-        }
-    }
-
-    pub fn with_capacity(capacity: u32, hasher: H) -> Self {
-        PerfectHasher {
-            alloted: HashMap::with_capacity_and_hasher(
-                capacity as usize,
-                BuildNoHashHasher::default(),
-            ),
-            hasher,
-        }
-    }
-
-    pub fn unique_id(&mut self, content: C) -> u32 {
-        content.hash(&mut self.hasher);
-        let mut hash = self.hasher.finish() as u32;
-
-        let mut comparison = Equal;
-
-        loop {
-            let entry = self
-                .alloted
-                .entry(hash)
-                .and_modify(|cached| comparison = (*cached).cmp(&content));
-
-            match comparison {
-                Equal => {
-                    entry.or_insert(content);
-                    return hash;
+macro_rules! PerfectHasher {
+    ($size:ty) => {
+        impl<C, H> PerfectHasher<$size, C, H>
+        where
+            H: Hasher,
+            C: Hash + Ord,
+        {
+            pub fn new(hasher: H) -> Self {
+                PerfectHasher {
+                    alloted: IntMap::default(),
+                    hasher,
                 }
-                Less => hash += 1,
-                Greater => hash -= 1,
+            }
+
+            pub fn with_capacity(capacity: $size, hasher: H) -> Self {
+                PerfectHasher {
+                    alloted: HashMap::with_capacity_and_hasher(
+                        capacity as usize,
+                        BuildNoHashHasher::default(),
+                    ),
+                    hasher,
+                }
+            }
+
+            pub fn unique_id(&mut self, content: C) -> $size {
+                content.hash(&mut self.hasher);
+                let mut hash = self.hasher.finish() as $size;
+
+                let mut comparison = Equal;
+
+                loop {
+                    let entry = self
+                        .alloted
+                        .entry(hash)
+                        .and_modify(|cached| comparison = (*cached).cmp(&content));
+
+                    match comparison {
+                        Equal => {
+                            entry.or_insert(content);
+                            return hash;
+                        }
+                        Less => hash += 1,
+                        Greater => hash -= 1,
+                    }
+                }
+            }
+
+            pub fn dissociate(&mut self, id: $size) {
+                self.alloted.remove(&id);
             }
         }
-    }
-
-    pub fn dissociate(&mut self, id: u32) {
-        self.alloted.remove(&id);
-    }
+    };
 }
+
+PerfectHasher!(u8);
+PerfectHasher!(u16);
+PerfectHasher!(u32);
+PerfectHasher!(u64);
+PerfectHasher!(usize);
